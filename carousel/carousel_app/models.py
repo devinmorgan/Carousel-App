7dfbd9a954a2743f72393d8ef33f9c06b1f013da
Devin Morgan <devinm@mit.edu>
2017-02-25 16:07:15 -0500

    Filled in the models for carousel_app

--- carousel/carousel_app/models.py
+++ carousel/carousel_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Carousel(models.Model):
+    name = models.CharField()
+    image_count = models.IntegerField()
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Carousel: %s" % self.name
+
+class Image(models.Model):
+    name = models.CharField()
+    carousel = models.ForeignKey(Carousel, related_name="pictures")
+    carousel_order_index = models.IntegerField()
+    image_url = models.CharField()
+    date_created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "Image: %s" %self.name
