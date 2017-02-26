aa8e55238815dd336c60487fa6f537171de14e26
Devin Morgan <devinm@mit.edu>
2017-02-26 02:00:44 -0500

    removed comments

--- carousel/carousel_app/admin.py
+++ carousel/carousel_app/admin.py
@@ -3,11 +3,9 @@ from .models import Carousel, Image
 from .forms import CarouselAdminForm, ImageAdminForm
 
 class CarouselAdmin(admin.ModelAdmin):
-    # form to provide custom data validation for carousels in admin
     form = CarouselAdminForm
 
 class ImageAdmin(admin.ModelAdmin):
-    # form to provide custom data validation for carousels in admin
     form = ImageAdminForm
 
 admin.site.register(Carousel, CarouselAdmin)
