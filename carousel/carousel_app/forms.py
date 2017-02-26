85f63270998ac6d997163c478c258dd1824da2c3
Devin Morgan <devinm@mit.edu>
2017-02-26 02:00:19 -0500

    Added some comments to forms.py

--- carousel/carousel_app/forms.py
+++ carousel/carousel_app/forms.py
@@ -3,6 +3,22 @@ from .models import Carousel, Image
 from django.core.exceptions import ValidationError
 import re
 
+# form to provide custom data validation for carousels in admin
+class CarouselAdminForm(forms.ModelForm):
+
+    class Meta:
+        model = Carousel
+        exclude = ['created']
+
+    def clean_name(self):
+        # Ensure that carousel names only contain alphanumerics and underscores
+        if re.search('([^a-zA-Z0-9_]+)', self.cleaned_data['name']):
+            raise ValidationError(('Carousel names can only contain alphanumerics and underscores'))
+        else:
+            return self.cleaned_data['name']
+
+
+# form to provide custom data validation for images in admin
 class ImageAdminForm(forms.ModelForm):
 
     class Meta:
@@ -19,26 +35,12 @@ class ImageAdminForm(forms.ModelForm):
     def clean_carousel_position_index(self):
         if 'carousel' not in self.cleaned_data:
             raise ValidationError(('Invalid image position index'))
+        carousel_image_count = len(Image.objects.filter(carousel__name=self.cleaned_data['carousel']))
 
         # Ensure that the position index for an image is a valid index in the
         # image list for the carousel
-        carousel_image_count = len(Image.objects.filter(carousel__name=self.cleaned_data['carousel']))
         if self.cleaned_data['carousel_position_index'] < 0 or \
                         self.cleaned_data['carousel_position_index'] > carousel_image_count:
             raise ValidationError(('Invalid image position index'))
         else:
             return self.cleaned_data['carousel_position_index']
-
-
-class CarouselAdminForm(forms.ModelForm):
-
-    class Meta:
-        model = Carousel
-        exclude = ['created']
-
-    def clean_name(self):
-        # Ensure that carousel names only contain alphanumerics and underscores
-        if re.search('([^a-zA-Z0-9_]+)', self.cleaned_data['name']):
-            raise ValidationError(('Carousel names can only contain alphanumerics and underscores'))
-        else:
-            return self.cleaned_data['name']
