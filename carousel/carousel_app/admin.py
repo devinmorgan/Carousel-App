fd0c62825644a93b03c575ca43a98c7d113376ef
Devin Morgan <devinm@mit.edu>
2017-02-26 01:52:03 -0500

    Added comments to admin.py

--- carousel/carousel_app/admin.py
+++ carousel/carousel_app/admin.py
@@ -2,13 +2,13 @@ from django.contrib import admin
 from .models import Carousel, Image
 from .forms import CarouselForm, ImageForm
 
-
 class CarouselAdmin(admin.ModelAdmin):
+    # form to provide custom data validation for carousels in admin
     form = CarouselForm
 
 class ImageAdmin(admin.ModelAdmin):
+    # form to provide custom data validation for carousels in admin
     form = ImageForm
 
 admin.site.register(Carousel, CarouselAdmin)
 admin.site.register(Image, ImageAdmin)
-
